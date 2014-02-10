22dded59c419a8cb3b95e5507e177e4d79235d79
Nicklaus Liow <nicklaus.liow@gmail.com>
2014-02-10 11:28:55 -0800

    email re-formatted

--- src/email.js
+++ src/email.js
@@ -14,11 +14,11 @@ var Email = {
             from: process.env.REPLY_TO_EMAIL,
             subject: 'Thank you for backing Nothing To Hide!',
             text: 'Howdy '+pledge.backer.name+',\n\n'+
-                'I\'m glad you\'re on board for this adventure in open source, open design, and open funding.'+
+                'I\'m glad you\'re on board for this adventure in open source, open design, and open funding. '+
                 'Exciting things are coming your way, so keep your eyes peeled! Although not literally.\n\n'+
                 'Feel free to reply to me, if you\'ve got questions or just want to say hi.\n\n'+
                 'Your progress pledge page: '+DOMAIN_NAME+'/pledge/'+pledge._id+'\n\n'+
-                'Cheers, ~Nicky Case'
+                'Cheers,\n~Nicky Case'
         });
     },
 
@@ -28,10 +28,10 @@ var Email = {
             from: process.env.REPLY_TO_EMAIL,
             subject: 'Part of your Progress Pledge for Nothing To Hide has been claimed.',
             text: 'Howdy '+pledge.backer.name+',\n\n'+
-                'The "End of Campaign" stage of your pledge has been claimed!'+
+                'The "End of Campaign" stage of your pledge has been claimed! '+
                 'Thanks for sticking with me, I promise this\'ll be great. Feel free to reply anytime!\n\n'+
                 'Your Progress Pledge page: '+DOMAIN_NAME+'/pledge/'+pledge._id+'\n\n'+
-                'Cheers, ~Nicky Case'
+                'Cheers,\n~Nicky Case'
         });
     }
 
