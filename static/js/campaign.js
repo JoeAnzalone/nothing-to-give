421d418a3c22b3dae0046d0ea7831e31098b3d78
Nicklaus Liow <nicklaus.liow@gmail.com>
2014-02-11 05:15:32 -0800

    The video, yay

--- static/js/campaign.js
+++ static/js/campaign.js
@@ -14,20 +14,20 @@ function playVideo(){
 
 var player;
 var READY_TO_PLAY = false;
-//window.onload = function(){
+window.onload = function(){
 
-// Loads the IFrame Player API code asynchronously.
-/*var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);*/
+	// Loads the IFrame Player API code asynchronously.
+	var tag = document.createElement('script');
+	tag.src = "https://www.youtube.com/iframe_api";
+	var firstScriptTag = document.getElementsByTagName('script')[0];
+	firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-//};
+};
 // Creates an <iframe> (and YouTube player) after the API code downloads.
 function onYouTubeIframeAPIReady() {
 	player = new YT.Player('video_trailer',{
 		width:'800', height:'350',
-		videoId:'ZPiSh9aEzMA',
+		videoId:'lksc2WRUOdM',
 		playerVars:{ rel:0, showinfo:0, autohide:1 },
 		events:{ 'onReady':onPlayerReady, 'onStateChange':onStateChange }
 	});
