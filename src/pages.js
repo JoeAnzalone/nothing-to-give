950c77269c839eaddd0d1003f928a9f9a1af1068
Nicklaus Liow <nicklaus.liow@gmail.com>
2014-02-12 07:46:12 -0800

    leaderboard links

--- src/pages.js
+++ src/pages.js
@@ -1,3 +1,4 @@
+var Q = require("q");
 var mongo = require('mongodb').MongoClient,
     ObjectID = require('mongodb').ObjectID,
     MONGO_URI = process.env.MONGO_URI;
@@ -8,22 +9,24 @@ module.exports = {
 
     	// Homepage
         app.get("/",function(req,res){
-            mongo.connect(MONGO_URI, function(err, db) {
-                if(err){ db.close(); return console.error(err); }
-                db.collection('pledges').find({cancelled:false}).toArray(function(err,pledges){
-                    
-                    db.close();
-                    if(err) { return console.error(err); }
-                
-                    res.render("Campaign.ejs",{
-                        pledges: pledges,
-                        campaign: {
-                            goal: 40000 // Hard coded
-                        }
-                    });
-
+            _getCampaignPledges().done(function(pledges){
+                res.render("Campaign.ejs",{
+                    pledges: pledges,
+                    campaign: {
+                        goal: 40000 // Hard coded
+                    }
                 });
             });
+        });
+
+        // Pledge Page
+        app.get("/pledge",function(req,res){
+            //_getCampaignPledges().done(function(stats){
+            res.render("PledgeCreate.ejs",{
+                //pledges: pledges,
+                STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY
+            });
+            //});
         });
 
         // Just the campaign (test purposes)
@@ -34,12 +37,19 @@ module.exports = {
             });
         });
 
-        // Pledge Page
-        app.get("/pledge",function(req,res){
-    		res.render("PledgeCreate.ejs",{
-                STRIPE_PUBLIC_KEY: process.env.STRIPE_PUBLIC_KEY
+        // Helper - get stats
+        var _getCampaignPledges = function(){
+            var deferred = Q.defer();
+            mongo.connect(MONGO_URI, function(err, db) {
+                if(err){ db.close(); return console.error(err); }
+                db.collection('pledges').find({cancelled:false}).toArray(function(err,pledges){
+                    db.close();
+                    if(err) { return console.error(err); }
+                    deferred.resolve(pledges);
+                });
             });
-        });
+            return deferred.promise;
+        }
 
     }
 
